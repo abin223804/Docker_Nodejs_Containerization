e5cfd3e23d257034b981f691dc2897d10dbd1987
Abin_haashIndia <haashtechnologies@gmail.com>
2024-11-27 10:17:34 +0530

    Dockerized

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.json({
+    "message": "Hello Abin!"
+  });
+});
+ 
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
